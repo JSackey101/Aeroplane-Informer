c756fb508aa0461566c02fe650bc817af153eb63
Chris Owen <chris@sigmalabs.xyz>
2023-03-01 10:14:58 +0000

    Update airports.py

--- airports.py
+++ airports.py
@@ -6,48 +6,58 @@ from rich.console import Console
 console = Console()
 
 
-def loadWeatherForLocation(lat, lng):
-    # Given a location, load the current weather for that location
+def loadWeatherForLocation(lat: str, lng: str) -> dict:
+    """Given a location, load the current weather for that location"""
+
     return {}
 
 
-def renderFlights(flights):
-    # Render a list of flights to the console using the Rich Library
-    #
-    # Consider using Panels, Grids, Tables or any of the more advanced
-    # features of the library
+def renderFlights(flights: list) -> None:
+    """Render a list of flights to the console using the Rich Library
+
+    Consider using Panels, Grids, Tables or any of the more advanced
+    features of the library"""
+
     console.print(flights)
 
 
-def getFlightsFromIata(iata):
-    # Given an IATA get the flights that are departing from that airport from Airlabs
-    return {}
-
-
-def loadAirportJSON():
-    # Load airport data from airports.json
-    return []
-
-
-def findAirportsFromName(name, airportData):
-    # Find an airport from the airportData given a name
-    # Could return one or more airport objects
+def getFlightsFromIata(iata: str) -> list:
+    """Given an IATA get the flights that are departing from that airport from Airlabs"""
 
     return []
 
 
-def findAirportFromIata(iata, airportData):
-    # Find an airport from the airportData given a name
-    # Should return exactly one airport object
+def loadAirportJSON() -> list:
+    """Load airport data from airports.json"""
+
+    return []
+
+
+def findAirportsFromName(name: str, airportData: list) -> list:
+    """
+    Find an airport from the airportData given a name
+    Could return one or more airport objects
+    """
+
+    return []
+
+
+def findAirportFromIata(iata: str, airportData: list) -> dict:
+    """
+    Find an airport from the airportData given a name
+    Should return exactly one airport object
+    """
 
     return {}
 
 
-def getSearch():
+def getSearch() -> str:
     return Prompt.ask("Search for an an airport")
 
 
-def main():
+def main() -> None:
+    """Repeatedly prompts the user for airport names and displays the result."""
+
     console.print(" ")
     console.print("✈️ ✈️ ✈️ ✈️ ✈️ ✈️ ✈️ ✈️")
     console.print("Welcome to the Airports Informer Tool")
