029970a6e952c4fbe50c6cddc43a1a853bb423a3
Christopher Owen <chris.owen@sigmalabs.co.uk>
2024-09-18 10:57:31 +0100

    Update airports.py

--- airports.py
+++ airports.py
@@ -9,8 +9,7 @@ console = Console()
 
 def load_weather_for_location(lat: str, lng: str) -> dict:
     """Given a location, load the current weather for that location"""
-
-    pass
+    ...
 
 
 def render_flights(flights: list) -> None:
@@ -24,14 +23,12 @@ def render_flights(flights: list) -> None:
 
 def get_flights_from_iata(iata: str) -> list:
     """Given an IATA get the flights that are departing from that airport from Airlabs"""
-
-    pass
+    ...
 
 
 def load_airport_JSON() -> list[dict]:
     """Returns airport information loaded from a file."""
-
-    pass
+    ...
 
 
 def find_airports_from_name(name: str, airport_data: list) -> list:
@@ -39,17 +36,7 @@ def find_airports_from_name(name: str, airport_data: list) -> list:
     Find an airport from the airport_data given a name
     Could return one or more airport objects
     """
-
-    pass
-
-
-def find_airport_from_iata(iata: str, airport_data: list) -> dict:
-    """
-    Find an airport from the airport_data given a name
-    Should return exactly one airport object
-    """
-
-    pass
+    ...
 
 if __name__ == "__main__":
     airport_data = load_airport_JSON()
