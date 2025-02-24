0665f5f5886183f1bd1dbcd2294f8b183759223c
JSackey101 <jeffsackey101@gmail.com>
2025-02-24 23:34:14 +0000

    Updated tests.

--- test_airports.py
+++ test_airports.py
@@ -1,10 +1,15 @@
-# pylint: skip-file
 
-import pytest
-from airports import AIRLABS_API_KEY, WEATHER_API_KEY, load_weather_for_location, flight_data_cleaner, add_weather_to_flights, round_to_hour, get_flights_from_iata, find_airport_from_iata, choose_desired_airport
-import requests_mock
-import requests
+from os import environ
 from datetime import datetime
+import pytest
+import requests_mock
+from airports import (load_weather_for_location, flight_data_cleaner, add_weather_to_flights,
+                      round_to_nearest_hour, get_flights_from_iata,
+                      find_airport_from_iata, choose_desired_airport, remove_minutes)
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(".env")
 
 UNCLEAN_FLIGHT_DATA = {
     'airline_iata': 'OP',
@@ -111,9 +116,11 @@ class TestLoadWeather():
 
     @staticmethod
     def test_calls_requests_get_method(requests_mock):
-        print(f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q=1,1&unixdt=1&aqi=yes")
+        """ Test to see whether a GET request is called. """
+        print(
+            f"http://api.weatherapi.com/v1/forecast.json?key={environ["WEATHER_API_KEY"]}&q=1,1&unixdt=1&aqi=yes")
         requests_mock.get(
-            f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q=1,1&unixdt=1262304000&aqi=yes", status_code=200, json={})
+            f"http://api.weatherapi.com/v1/forecast.json?key={environ["WEATHER_API_KEY"]}&q=1,1&unixdt=1262304000&aqi=yes", status_code=200, json={})
         load_weather_for_location(1, 1, 1262304000)
 
         assert requests_mock.called
@@ -122,48 +129,57 @@ class TestLoadWeather():
 
     @staticmethod
     def test_http_error_raised(requests_mock):
+        """ Test to see whether a HTTPError is raised if the GET request fails. """
         with pytest.raises(requests.HTTPError):
             requests_mock.get(
-                f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q=1,1&unixdt=1262304000&aqi=yes", status_code=404, json={})
+                f"http://api.weatherapi.com/v1/forecast.json?key={environ["WEATHER_API_KEY"]}&q=1,1&unixdt=1262304000&aqi=yes", status_code=400, json={'error':{'message':'Bad API key'}})
             load_weather_for_location(1, 1, 1262304000)
 
     @staticmethod
     def test_non_number_lat():
+        """ Test to see whether a TypeError is raised when the latitude is not a number. """
         with pytest.raises(TypeError):
             load_weather_for_location("test", 1, 1262304000)
 
     @staticmethod
     def test_non_number_lng():
+        """ Test to see whether a TypeError is raised when the longitude is not a number. """
         with pytest.raises(TypeError):
             load_weather_for_location(1, "test", 1262304000)
 
     @staticmethod
     def test_non_number_timestamp():
+        """ Test to see whether a TypeError is raised when the timestamp is not a number. """
         with pytest.raises(TypeError):
             load_weather_for_location(1, 1, "test")
 
     @staticmethod
     def test_timestamp_too_early():
+        """ Test to see whether a ValueError is raised when the timestamp is before 1st Jan 2010. """
         with pytest.raises(ValueError):
             load_weather_for_location(1, 1, 1262300000)
 
     @staticmethod
     def test_lat_too_high():
+        """ Test to see whether a ValueError is raised when the latitude is too high. """
         with pytest.raises(ValueError):
             load_weather_for_location(90.5, 1, 1362300000)
 
     @staticmethod
     def test_lat_too_low():
+        """ Test to see whether a ValueError is raised when the latitude is too low. """
         with pytest.raises(ValueError):
             load_weather_for_location(-90.5, 1, 1362300000)
 
     @staticmethod
     def test_lng_too_high():
+        """ Test to see whether a ValueError is raised when the longitude is too high. """
         with pytest.raises(ValueError):
             load_weather_for_location(1, 180.5, 1362300000)
 
     @staticmethod
     def test_lng_too_low():
+        """ Test to see whether a ValueError is raised when the longitude is too low. """
         with pytest.raises(ValueError):
             load_weather_for_location(1, -180.5, 1362300000)
 
@@ -171,20 +187,24 @@ class TestFlightDataCleaner():
     """ A class to test the flight_data_cleaner function. """
     @staticmethod
     def test_flight_data_correctly_cleaned():
+        """ Test to see whether the flight data is correctly returned with only desired components. """
         assert flight_data_cleaner([UNCLEAN_FLIGHT_DATA]) == [CLEAN_FLIGHT_DATA]
 
     @staticmethod
     def test_error_raised_if_no_flights():
+        """ Test to see whether a ValueError is raised when there are no flights given. """
         with pytest.raises(ValueError):
             flight_data_cleaner([])
     
     @staticmethod
     def test_error_raised_if_non_list_given():
+        """ Test to see whether a TypeError is raised when the flight data given is not a list. """
         with pytest.raises(TypeError):
             flight_data_cleaner(UNCLEAN_FLIGHT_DATA)
 
     @staticmethod
     def test_error_raised_if_flights_not_dict():
+        """ Test to see whether a TypeError is raised when the flights within the flight data are not dicts. """
         with pytest.raises(TypeError):
             flight_data_cleaner(['test', 'test2'])
 
@@ -192,36 +212,63 @@ class TestAddWeather():
     """ A class to test the add_weather_to_flights function. """
     @staticmethod
     def test_error_raised_if_no_flights():
+        """ Test to see whether a ValueError is raised when there are no flights given. """
         with pytest.raises(ValueError):
             add_weather_to_flights([])
 
     @staticmethod
     def test_error_raised_if_non_list_given():
+        """ Test to see whether a TypeError is raised when the flight data given is not a list. """
         with pytest.raises(TypeError):
             add_weather_to_flights(UNCLEAN_FLIGHT_DATA)
 
     @staticmethod
     def test_error_raised_if_flights_not_dict():
+        """ Test to see whether a TypeError is raised when the flights within the flight data are not dicts. """
         with pytest.raises(TypeError):
             add_weather_to_flights(['test', 'test2'])
 
 class TestRoundHour():
-    """ A class to test the round_to_hour function. """
+    """ A class to test the round_to_nearest_hour function. """
     @staticmethod
-    def test_error_raised_if_flights_not_dict():
+    def test_error_raised_if_not_datetime():
+        """ Test to see whether a TypeError is raised when the input is not a datetime object. """
         with pytest.raises(TypeError):
-            round_to_hour('27/08/2002')
+            round_to_nearest_hour('27/08/2002')
 
     @staticmethod
-    def test_rounded_correctly():
-        assert round_to_hour(datetime(2002,8,27,13,35,0)) == datetime(2002,8,27,14,0,0)
+    def test_rounded_correctly_up():
+        """ Test to see whether the datetime object is correctly rounded to the nearest hour upwards. """
+        assert round_to_nearest_hour(datetime(2002,8,27,13,35,0)) == datetime(2002,8,27,14,0,0)
+
+    @staticmethod
+    def test_rounded_correctly_down():
+        """ Test to see whether the datetime object is correctly rounded to the nearest hour downwards. """
+        assert round_to_nearest_hour(
+            datetime(2002, 8, 27, 13, 25, 0)) == datetime(2002, 8, 27, 13, 0, 0)
+
+
+class TestRemoveMinutes():
+    """ A class to test the round_to_nearest_hour function. """
+    @staticmethod
+    def test_error_raised_if_not_datetime():
+        """ Test to see whether a TypeError is raised when the input is not a datetime object."""
+        with pytest.raises(TypeError):
+            remove_minutes('27/08/2002')
+
+    @staticmethod
+    def test_removed_correctly():
+        """ Test to see whether the datetime object is correctly stripped of its minutes/seconds. """
+        assert remove_minutes(
+            datetime(2002, 8, 27, 13, 35, 0)) == datetime(2002, 8, 27, 13, 0, 0)
 
 class TestFlightsFromIATA():
     """ A class to test the get_flights_from_iata function. """
     @staticmethod
     def test_calls_requests_get_method(requests_mock):
+        """ Test to see whether a GET request is called. """
         requests_mock.get(
-            f"https://airlabs.co/api/v9/schedules?dep_iata=ABCD&api_key={AIRLABS_API_KEY}", status_code=200, json={})
+            f"https://airlabs.co/api/v9/schedules?dep_iata=ABCD&api_key={environ["AIRLABS_API_KEY"]}", status_code=200, json={})
         get_flights_from_iata("ABCD")
 
         assert requests_mock.called
@@ -230,13 +277,15 @@ class TestFlightsFromIATA():
 
     @staticmethod
     def test_http_error_raised(requests_mock):
+        """ Test to see whether a HTTPError is raised if the GET request fails. """
         with pytest.raises(requests.HTTPError):
             requests_mock.get(
-                f"https://airlabs.co/api/v9/schedules?dep_iata=ABCD&api_key={AIRLABS_API_KEY}", status_code=404, json={})
+                f"https://airlabs.co/api/v9/schedules?dep_iata=ABCD&api_key={environ["AIRLABS_API_KEY"]}", status_code=404, json={'error': {'message': 'Bad API key'}})
             get_flights_from_iata("ABCD")
 
     @staticmethod
     def test_error_raised_if_non_string():
+        """ Test to see whether a TypeError is raised when the input is not a string. """
         with pytest.raises(TypeError):
             get_flights_from_iata(192)
 
@@ -245,8 +294,9 @@ class TestAirportFromIATA():
     """ A class to test the find_airport_from_iata function. """
     @staticmethod
     def test_calls_requests_get_method(requests_mock):
+        """ A class to test the get_flights_from_iata function. """
         requests_mock.get(
-            f"https://airlabs.co/api/v9/airports?iata_code=ABCD&api_key={AIRLABS_API_KEY}", status_code=200, json={'response': [0]})
+            f"https://airlabs.co/api/v9/airports?iata_code=ABCD&api_key={environ["AIRLABS_API_KEY"]}", status_code=200, json={'response': [0]})
         find_airport_from_iata("ABCD")
 
         assert requests_mock.called
@@ -255,13 +305,15 @@ class TestAirportFromIATA():
 
     @staticmethod
     def test_http_error_raised(requests_mock):
+        """ Test to see whether a HTTPError is raised if the GET request fails. """
         with pytest.raises(requests.HTTPError):
             requests_mock.get(
-                f"https://airlabs.co/api/v9/airports?iata_code=ABCD&api_key={AIRLABS_API_KEY}", status_code=404, json={'response': [0]})
+                f"https://airlabs.co/api/v9/airports?iata_code=ABCD&api_key={environ["AIRLABS_API_KEY"]}", status_code=404, json={'error': {'message': 'Bad API key'}})
             find_airport_from_iata("ABCD")
 
     @staticmethod
     def test_error_raised_if_non_string():
+        """ Test to see whether a TypeError is raised when the input is not a string. """
         with pytest.raises(TypeError):
             find_airport_from_iata(192)
 
@@ -270,5 +322,6 @@ class TestChooseDesiredAirport():
 
     @staticmethod
     def test_error_raised():
+        """ Test to see whether a ValueError is raised when the airport matches list is empty. """
         with pytest.raises(ValueError):
             choose_desired_airport([])
